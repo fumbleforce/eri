2bdc5d79f83387cb849cd16f1a2f477be5f92bce
jorgeer <jfemail3@gmail.com>
2014-06-23 16:39:44 +0200

    stuff was deprecated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log('Environment set to '+process.env.NODE_ENV);
 
 // Imports
 var express     = require('express'),
+    bodyParser  = require('body-parser'),
     _           = require('underscore'),
     routes      = require('./conf/routes').router,
     ioRoutes    = require('./conf/routes').ioRouter,
@@ -16,21 +17,15 @@ app.set('views', __dirname + '/lib/views');
 app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
 app.engine('.jade', require('jade').__express);
-app.use(express.bodyParser());
-app.use(express.logger('dev'));
-app.use(express.methodOverride());
-app.use(express.cookieParser());
-app.use(express.cookieSession({
-    secret: 'cult master omega destruction incantation',
-    cookie: { maxAge: 1000*60*60 }
-}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 
 app.use(express.static(__dirname + '/public'));
-
-console.log('configuring');
-app.configure('development', function(){ config.setDevConf(app); });
-app.configure('production', function(){ config.setProdConf(app); });
-app.configure('test', function(){ config.setTestConf(app); });
 
 console.log('routing');
 // Routing setup
@@ -46,7 +41,7 @@ console.log('routing');
 
 console.log('listening');
 var server = require('http').createServer(app);
-server.listen(app.get('port'), function(){ console.log('Listening to '+app.get('port')) });
+server.listen(app.get('port'), function(){ console.log('Listening to '+app.get('port')); });
 var socket_listener = require('socket.io').listen(server, { log:true });
 socket_listener.set("transports", ["xhr-polling", "jsonp-polling"]);
 socket_listener.sockets.on('connection', ioRoutes);
